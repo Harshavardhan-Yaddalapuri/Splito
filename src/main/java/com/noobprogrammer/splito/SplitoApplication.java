1b7b0177fd896797128d6611c8f6a40b73ab8ff0
Harshavardhan <haarshavardhan08@gmail.com>
2025-01-31 00:49:30 -0600

    Task-1.1: Project Setup

---
+++ src/main/java/com/noobprogrammer/splito/SplitoApplication.java
@@ -0,0 +1,13 @@
+package com.noobprogrammer.splito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noobprogrammer/splito/SplitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobprogrammer.splito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
